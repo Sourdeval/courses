4e224b56f0d26662485a9beb9c10cbc9c2c233bf
Sourdeval <sourdeval@laposte.net>
2023-03-18 16:28:42 +0100

    List of products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'courses-app';
+
+  products: Product[] | undefined;
+
+  constructor(){
+    this.products = [
+      {
+        "name":"tomates",
+        "buy": false
+      },
+      {
+        "name":"PQ",
+        "buy": true
+      },{
+        "name":"pain",
+        "buy": false
+      },{
+        "name":"biscuits chocolat",
+        "buy": false
+      },{
+        "name":"jambon",
+        "buy": true
+      },
+    ]
+  }
+
+
+  getOpacity(p:Product){
+    if (p.buy){
+      return "background: rgba(120, 120, 120, 0.7);"
+    }
+  }
 }
